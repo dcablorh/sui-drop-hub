e1ac97ef84c2091503e14a1a3743384cba1001c9
dcablorh <dcaborh@gmail.com>
2025-09-09 16:15:08 -0700

    changed package id

--- src/lib/suiClient.ts
+++ src/lib/suiClient.ts
@@ -3,8 +3,8 @@ import { SuiClient, getFullnodeUrl } from '@mysten/sui.js/client';
 export const suiClient = new SuiClient({ url: getFullnodeUrl('testnet') });
 
 // Contract constants
-export const REGISTRY_ID = '0x8ceec78670c97b8b01a0d50566a85177b8f910527759d93ef50486ef8c10f2e1';
-export const PACKAGE_ID = '0x16c8dd907e254555c7a5d592f7b10b6040e19d07be94fc4adc6cdb7138011a55';
+export const REGISTRY_ID = '0x8b6529443d0cb9cd4c77b802de5af69e83152ad39ab738445ce7f899da73aeaf';
+export const PACKAGE_ID = '0x4b2e085965a7ca0d29b6da1022353d50d71d55d42e170ccde6b528444da6b33c';
 export const MODULE = 'dropnew';
 export const COIN_TYPE = '0x2::sui::SUI';
 export const CLOCK_ID = '0x6';
